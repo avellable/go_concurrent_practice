e5c3c7ceabca43da20a10771b5166a0fc4e37e12
Mahesh Baheti <mahsh.baheti@gmail.com>
2015-05-23 22:45:08 +0900

    Add closure.go

---
+++ closures/closure.go
@@ -0,0 +1,17 @@
+import(
+  "fmt"
+  "strings"
+)
+
+
+func main() {
+  
+  myString := shortenString("They say married men don't invent things ...")
+
+  fmt.Println(myString())
+  fmt.Println(myString())  
+  fmt.Println(myString())  
+  fmt.Println(myString())  
+  fmt.Println(myString())  
+  fmt.Println(myString())
+}
