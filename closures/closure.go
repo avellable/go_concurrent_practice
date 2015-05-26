885d2eb89ac364bcc5fb1421f3161357c5cb16da
Mahesh Baheti <mahsh.baheti@gmail.com>
2015-05-26 09:57:40 +0900

    Correct indentations

--- closures/closure.go
+++ closures/closure.go
@@ -1,31 +1,32 @@
-import(
-  "fmt"
-  "strings"
+package main
+
+import (
+	"fmt"
+	"strings"
 )
 
 func shortenString(message string) func() string {
-  
-  return func() string {
-    messageSlice := strings.Split(message," ")
-    wordLength := len(messageSlice)
-    if wordLength < 1 {
-      return "Nothingn Left!"
-    }else {
-      messageSlice = messageSlice[:(wordLength-1)]
-      message = strings.Join(messageSlice, " ")
-      return message
-    }
-  }
+	return func() string {
+		messageSlice := strings.Split(message, " ")
+		wordLength := len(messageSlice)
+		if wordLength < 1 {
+			return "Nothingn Left!"
+		} else {
+			messageSlice = messageSlice[:(wordLength - 1)]
+			message = strings.Join(messageSlice, " ")
+			return message
+		}
+	}
 }
 
 func main() {
-  
-  myString := shortenString("They say married men don't invent things ...")
 
-  fmt.Println(myString())
-  fmt.Println(myString())  
-  fmt.Println(myString())  
-  fmt.Println(myString())  
-  fmt.Println(myString())  
-  fmt.Println(myString())
+	myString := shortenString("They say married men don't invent things ...")
+
+	fmt.Println(myString())
+	fmt.Println(myString())
+	fmt.Println(myString())
+	fmt.Println(myString())
+	fmt.Println(myString())
+	fmt.Println(myString())
 }
--- rss/rss.go
+++ rss/rss.go
@@ -1,31 +1,31 @@
 package main
 
-import(
-  "github.com/ajstarks/svgo"
-  rss "github.com/jteeuwen/go-pkg-rss"    
-  "net/http"
-  "log"
-  "fmt"
-  "strconv"
-  "time"
-  "os"
-  "sync"
-  "runtime"
+import (
+	"fmt"
+	"github.com/ajstarks/svgo"
+	rss "github.com/jteeuwen/go-pkg-rss"
+	"log"
+	"net/http"
+	"os"
+	"runtime"
+	"strconv"
+	"sync"
+	"time"
 )
 
 type Feed struct {
-  url string
-  status int
-  itemCount int
-  complete bool
-  itemsComplete bool
-  index int
+	url           string
+	status        int
+	itemCount     int
+	complete      bool
+	itemsComplete bool
+	index         int
 }
 
 type FeedItem struct {
-  feedIndex int
-  complete bool
-  url string
+	feedIndex int
+	complete  bool
+	url       string
 }
 
 var feeds []Feed
@@ -40,54 +40,50 @@ var wg sync.WaitGroup
 
 func grabFeed(feed *Feed, feedChan chan bool, osvg *svg.SVG) {
 
+	startGrab := time.Now().Unix()
+	startGrabSeconds := startGrab - startTime
 
-  startGrab := time.Now().Unix()
-  startGrabSeconds := startGrab - startTime
+	fmt.Println("Grabbing feed", feed.url, "at", startGrabSeconds, "second mark")
 
-  fmt.Println("Grabbing feed",feed.url,"at",startGrabSeconds,"second mark")
+	if feed.status == 0 {
+		fmt.Println("Feed not yet read")
+		feed.status = 1
 
-  if feed.status == 0 {
-    fmt.Println("Feed not yet read")
-    feed.status = 1
+		startX := int(startGrabSeconds * 33)
+		startY := feedSpace * (feed.index)
 
-    startX := int(startGrabSeconds * 33);
-    startY := feedSpace * (feed.index)
+		fmt.Println(startY)
+		wg.Add(1)
 
-    fmt.Println(startY)
-    wg.Add(1)
+		rssFeed := rss.New(timeout, true, channelHandler,
+			itemsHandler)
 
-    rssFeed := rss.New(timeout, true, channelHandler, 
-      itemsHandler);
+		if err := rssFeed.Fetch(feed.url, nil); err != nil {
+			fmt.Fprintf(os.Stderr, "[e] %s: %s", feed.url, err)
+			return
+		} else {
 
-    if err := rssFeed.Fetch(feed.url, nil); err != nil {
-      fmt.Fprintf(os.Stderr, "[e] %s: %s", feed.url, err)
-      return
-    } else {
+			endSec := time.Now().Unix()
+			endX := int((endSec - startGrab))
+			if endX == 0 {
+				endX = 1
+			}
+			fmt.Println("Read feed in", endX, "seconds")
+			osvg.Rect(startX, startY, endX, feedSpace, "fill: #000000;opacity:.4")
+			wg.Wait()
 
+			endGrab := time.Now().Unix()
+			endGrabSeconds := endGrab - startTime
+			feedEndX := int(endGrabSeconds * 33)
 
+			osvg.Rect(feedEndX, startY, 1, feedSpace, "fill:#ff0000;opacity:.9")
 
-      endSec := time.Now().Unix()    
-      endX := int( (endSec - startGrab) )
-      if endX == 0 {
-        endX = 1
-      }
-      fmt.Println("Read feed in",endX,"seconds")
-      osvg.Rect(startX,startY,endX,feedSpace,"fill: #000000;opacity:.4")
-      wg.Wait()
-      
-      endGrab := time.Now().Unix()
-      endGrabSeconds := endGrab - startTime
-      feedEndX := int(endGrabSeconds * 33);      
+			feedChan <- true
+		}
 
-      osvg.Rect(feedEndX,startY,1,feedSpace,"fill:#ff0000;opacity:.9")
-
-      feedChan <- true
-    }
-
-
-  }else if feed.status == 1{
-    fmt.Println("Feed already in progress")
-  }
+	} else if feed.status == 1 {
+		fmt.Println("Feed already in progress")
+	}
 
 }
 
@@ -97,79 +93,76 @@ func channelHandler(feed *rss.Feed, newchannels []*rss.Channel) {
 
 func itemsHandler(feed *rss.Feed, ch *rss.Channel, newitems []*rss.Item) {
 
-  fmt.Println("Found",len(newitems),"items in",feed.Url)
+	fmt.Println("Found", len(newitems), "items in", feed.Url)
 
+	for i := range newitems {
+		url := *newitems[i].Guid
+		fmt.Println(url)
 
-  for i := range newitems {
-    url := *newitems[i].Guid
-    fmt.Println(url)
+	}
 
-  }
-
-  wg.Done()
+	wg.Done()
 }
 
 func getRSS(rw http.ResponseWriter, req *http.Request) {
-  startTime = time.Now().Unix()  
-  rw.Header().Set("Content-Type", "image/svg+xml")
-  outputSVG := svg.New(rw)
-  outputSVG.Start(width, height)
+	startTime = time.Now().Unix()
+	rw.Header().Set("Content-Type", "image/svg+xml")
+	outputSVG := svg.New(rw)
+	outputSVG.Start(width, height)
 
-  feedSpace = (height-20) / len(feeds)
+	feedSpace = (height - 20) / len(feeds)
 
-  for i:= 0; i < 30000; i++ {
-    timeText := strconv.FormatInt(int64(i/10),10)
-    if i % 1000 == 0 {
-      outputSVG.Text(i/30,390,timeText,"text-anchor:middle;font-size:10px;fill:#000000")      
-    }else if i % 4 == 0 {
-      outputSVG.Circle(i,377,1,"fill:#cccccc;stroke:none")  
-    }
+	for i := 0; i < 30000; i++ {
+		timeText := strconv.FormatInt(int64(i/10), 10)
+		if i%1000 == 0 {
+			outputSVG.Text(i/30, 390, timeText, "text-anchor:middle;font-size:10px;fill:#000000")
+		} else if i%4 == 0 {
+			outputSVG.Circle(i, 377, 1, "fill:#cccccc;stroke:none")
+		}
 
+		if i%10 == 0 {
+			outputSVG.Rect(i, 0, 1, 400, "fill:#dddddd")
+		}
+		if i%50 == 0 {
+			outputSVG.Rect(i, 0, 1, 400, "fill:#cccccc")
+		}
 
-    if i % 10 == 0 {
-      outputSVG.Rect(i,0,1,400,"fill:#dddddd")
-    }
-    if i % 50 == 0 {
-      outputSVG.Rect(i,0,1,400,"fill:#cccccc")
-    }
-  
+	}
 
-  }
+	feedChan := make(chan bool, 3)
 
-  feedChan := make(chan bool, 3)
+	for i := range feeds {
 
-  for i := range feeds {
+		outputSVG.Rect(0, (i * feedSpace), width, feedSpace,
+			"fill:"+colors[i]+";stroke:none;")
+		feeds[i].status = 0
+		go grabFeed(&feeds[i], feedChan, outputSVG)
+		<-feedChan
+	}
 
-    outputSVG.Rect(0, (i*feedSpace), width, feedSpace, 
-      "fill:"+colors[i]+";stroke:none;")
-    feeds[i].status = 0
-    go grabFeed(&feeds[i], feedChan, outputSVG)
-    <- feedChan
-  }
-
-  outputSVG.End()
+	outputSVG.End()
 }
 
 func main() {
 
-  runtime.GOMAXPROCS(2)
+	runtime.GOMAXPROCS(2)
 
-  timeout = 1000
+	timeout = 1000
 
-  width = 1000
-  height = 400
+	width = 1000
+	height = 400
 
-  feeds = append(feeds, Feed{index: 0, url: "https://groups.google.com/forum/feed/golang-nuts/msgs/rss_v2_0.xml?num=50", status: 0, itemCount: 0, complete: false, itemsComplete: false})
-  feeds = append(feeds, Feed{index: 1, url: "http://www.reddit.com/r/golang/.rss", status: 0, itemCount: 0, complete: false, itemsComplete: false})
-  feeds = append(feeds, Feed{index: 2, url: "https://groups.google.com/forum/feed/golang-dev/msgs/rss_v2_0.xml?num=50", status: 0, itemCount: 0, complete: false, itemsComplete: false })
+	feeds = append(feeds, Feed{index: 0, url: "https://groups.google.com/forum/feed/golang-nuts/msgs/rss_v2_0.xml?num=50", status: 0, itemCount: 0, complete: false, itemsComplete: false})
+	feeds = append(feeds, Feed{index: 1, url: "http://www.reddit.com/r/golang/.rss", status: 0, itemCount: 0, complete: false, itemsComplete: false})
+	feeds = append(feeds, Feed{index: 2, url: "https://groups.google.com/forum/feed/golang-dev/msgs/rss_v2_0.xml?num=50", status: 0, itemCount: 0, complete: false, itemsComplete: false})
 
-  colors = append(colors,"#ff9999")
-  colors = append(colors,"#99ff99")
-  colors = append(colors,"#9999ff")  
+	colors = append(colors, "#ff9999")
+	colors = append(colors, "#99ff99")
+	colors = append(colors, "#9999ff")
 
-  http.Handle("/getrss", http.HandlerFunc(getRSS))
-    err := http.ListenAndServe(":1900", nil)
-    if err != nil {
-        log.Fatal("ListenAndServe:", err)
-    }  
+	http.Handle("/getrss", http.HandlerFunc(getRSS))
+	err := http.ListenAndServe(":1900", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
 }
